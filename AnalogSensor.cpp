93ea6565911122d154d3e3afe944027189bc9b5a
pinchy <sebeckmas@gmail.com>
2022-11-17 23:08:30 +1100

    basic workflow and state machine

---
+++ AnalogSensor.cpp
@@ -0,0 +1,79 @@
+#include <AnalogSensor.h>
+
+
+AnalogSensor::AnalogSensor() {}
+
+AnalogSensor::AnalogSensor(int p) { this->setPin(p); }
+AnalogSensor::~AnalogSensor() {}
+
+void AnalogSensor::begin(void)
+{
+    ;
+    ;
+}
+
+void AnalogSensor::setUpperThreshold(uint16_t threshold, CallBack_t cb)
+{
+    this->_upperThreshold = threshold;
+    this->_CallBackOnUpperThreshold = cb;
+}
+void AnalogSensor::setLowerThreshold(uint16_t threshold, CallBack_t cb)
+{
+    this->_lowerThreshold = threshold;
+    this->_CallBackOnLowerThreshold = cb;
+}
+
+void AnalogSensor::setTimeBetweenReads(uint32_t timeout) { this->_timeBetweenReads = timeout; }
+
+
+void AnalogSensor::setPin(int p)
+{
+    this->_pin = p;
+    pinMode(this->_pin, INPUT);
+}
+
+
+uint16_t AnalogSensor::read(void)
+{
+    this->_timeOfLastRead = millis();
+    this->_raw = analogRead(this->_pin);
+    return map(this->_raw, this->_fromLow, this->_fromHigh, this->_toLow, this->_toHigh);
+}
+
+
+void AnalogSensor::setBounds(uint16_t fromLow, uint16_t fromHigh, uint16_t toLow, uint16_t toHigh)
+{
+    this->_fromLow = fromLow;
+    this->_fromHigh = fromHigh;
+    this->_toLow = toLow;
+    this->_toHigh = toHigh;
+}
+void AnalogSensor::setBounds(uint16_t toLow, uint16_t toHigh)
+{
+    this->_toLow = toLow;
+    this->_toHigh = toHigh;
+}
+
+
+void AnalogSensor::tick(void)
+{
+    if (millis() - this->_timeOfLastRead > this->_timeBetweenReads)
+    {
+        uint16_t val = this->read();
+        this->_checkThresholds(val);
+    }
+}
+
+
+void AnalogSensor::_checkThresholds(uint16_t val)
+{
+    if (this->_CallBackOnLowerThreshold != nullptr)
+    {
+        if (val > this->_lowerThreshold) this->_CallBackOnLowerThreshold();
+    }
+
+    if (this->_CallBackOnUpperThreshold != nullptr)
+    {
+        if (val > this->_upperThreshold) this->_CallBackOnUpperThreshold();
+    }
+}
---
+++ AnalogSensor.h
@@ -0,0 +1,47 @@
+#ifndef ANALOG_SENSOR_H
+#define ANALOG_SENSOR_H
+
+#include <Arduino.h>
+
+class AnalogSensor
+{
+        typedef void (*CallBack_t)();
+
+    private:
+        int _pin;
+        uint16_t _raw;
+        uint32_t _timeBetweenReads = 1000;
+        uint32_t _timeOfLastRead = 0;
+
+        uint16_t _upperThreshold;
+        uint16_t _lowerThreshold;
+
+        uint16_t _fromLow = 0;
+        uint16_t _fromHigh = 1024;
+        uint16_t _toLow = 0;
+        uint16_t _toHigh = 1024;
+
+        CallBack_t _CallBackOnUpperThreshold = nullptr;
+        CallBack_t _CallBackOnLowerThreshold = nullptr;
+
+        void _checkThresholds(uint16_t val);
+
+    public:
+        AnalogSensor();
+        AnalogSensor(int p);
+        ~AnalogSensor();
+        void begin(void);
+
+        void setUpperThreshold(uint16_t threshold, CallBack_t cb);
+        void setLowerThreshold(uint16_t threshold, CallBack_t cb);
+
+        void setBounds(uint16_t fromLow, uint16_t fromHigh, uint16_t toLow, uint16_t toHigh);
+        void setBounds(uint16_t toLow, uint16_t toHigh);
+
+        void setTimeBetweenReads(uint32_t timeout);
+        void setPin(int p);
+        uint16_t read(void);
+        void tick(void);
+};
+
+#endif
