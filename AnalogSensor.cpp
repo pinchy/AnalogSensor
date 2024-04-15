afc44ed2f856108a6fb272cd107b542ac1cca686
pinchy <sebeckmas@gmail.com>
2024-04-15 14:00:16 +1000

    even more merge madness

--- AnalogSensor.cpp
+++ AnalogSensor.cpp
@@ -124,3 +124,8 @@ void AnalogSensor::reset(void)
     this->_upperThresholdCallBackFired = false;
     this->_lowerThresholdCallBackFired = false;
 }
+
+float mapFloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
